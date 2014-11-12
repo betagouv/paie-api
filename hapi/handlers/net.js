a5285432e18159edb1142edeb5623035211c9598
Matti Schneider <matti.schneider@data.gouv.fr>
2014-11-12 18:18:50 +0100

    Add Hapi server
    Open /api/net route

---
+++ hapi/handlers/net.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Operations on /net
+ */
+module.exports = {
+
+	/**
+	 *
+	 * parameters: brut, imposable
+	 * produces: application/json
+	 */
+	get: function calculateNet(req, reply) {
+		reply().code(501);
+	}
+
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var Hapi = require('hapi'),
+	Swaggerize = require('swaggerize-hapi');
+
+var server = new Hapi.Server(8000);
+
+// The mount point is specified by the swagger's `basePath` property.
+server.pack.register({
+	plugin: Swaggerize,
+	options: {
+		api: require('./swagger.json'),
+		handlers: './hapi/handlers',
+		docspath: '/'
+	}
+}, function (error) {
+	server.start(function () {
+		server.plugins.swagger.setHost(server.info.host + ':' + server.info.port);
+	});
+});
+
+console.log('Server running on', server.info.host + ':' + server.info.port);
