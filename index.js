3f2e6fcb11ca7af9a56b469d2645087f536e8c3c
Matti Schneider <matti.schneider@data.gouv.fr>
2014-11-13 10:37:12 +0100

    Setup server port in NPM config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var Hapi = require('hapi'),
 	Swaggerize = require('swaggerize-hapi');
 
-var server = new Hapi.Server(8000);
+var server = new Hapi.Server(process.env.npm_package_config_port);
 
 // The mount point is specified by the swagger's `basePath` property.
 server.pack.register({
