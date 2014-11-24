261133612cdb3af2a1883d79efb7c74bf403a7fc
Matti Schneider <matti.schneider@data.gouv.fr>
2014-11-24 12:01:06 +0100

    Describe /fillon endpoint

---
+++ hapi/handlers/fillon.js
@@ -0,0 +1,13 @@
+/**
+ * Operations on /fillon
+ */
+module.exports = {
+	/**
+	 *
+	 * parameters: brut, categorie, taille
+	 * produces: application/json
+	 */
+	get: function calculateFillon(req, reply) {
+		reply().code(501);
+	}
+};
